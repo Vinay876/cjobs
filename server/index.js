d1bbbfba7b0ab2172e3228453f07b47e9ecddd29
Yash Sharma <yashsharma2493@gmail.com>
2022-05-29 13:41:14 +0530

    development server

--- server/index.js
+++ server/index.js
@@ -5,29 +5,30 @@ import dotenv from 'dotenv'
 import Connection from './connection/connection.js'
 import router from './routes/router.js';
 import path from 'path'
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
-dotenv.config({path:'./data.env'})
+dotenv.config({ path: './data.env' })
 
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,"../client/build/index.html")));
+app.use(express.static(__dirname));
 app.use(cors());
 app.use('/', router);
 app.use(express.json())
 
-
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname ,'../client/build/index.html'));
+// if (process.env.NODE_ENV == "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
   })
+// }
 
 Connection(process.env.DB_URL)
-app.listen(PORT,()=>console.log('Server is running at '+PORT))
+app.listen(PORT, () => console.log('Server is running at ' + PORT))
